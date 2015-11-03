10b9d7d02fe1e00f06d6616d3d5b9f45edc0a98d
97oskmal <oskarmalmquist@gmail.com>
2015-11-03 09:14:25 +0100

    Update __init__.py

--- exam1/__init__.py
+++ exam1/__init__.py
@@ -1,1 +1,27 @@
 
+import sys
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+import Model
+
+
+
+class Exam1(QMainWindow):
+  
+  def __init__(self, parent=None):
+        super(Exam1, self).__init__()
+        
+        self.setWindowTitle("Exam1 template")
+        self.initUI()
+
+  
+  def initUI(self):
+      
+  def run(self):
+        self.show()
+        sys.exit(app.exec_())
+      
+      
+      
+app = QApplication(sys.argv):
+Exam1().run()
